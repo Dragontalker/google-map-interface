d5a14340997d0c764581fbd1b0359c9349ac6c58
Richard <richard.yang.tong@gmail.com>
2021-04-16 19:11:30 -0400

    Added a constructor.

--- src/Company.ts
+++ src/Company.ts
@@ -6,5 +6,14 @@ export class Company {
   location: {
     lat: number;
     lng: number;
+  };
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
   }
 };
